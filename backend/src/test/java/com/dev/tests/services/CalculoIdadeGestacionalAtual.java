dd97bde2428c13ce5238db876b4de88484e84683
fhilips <fhilips.souza@gmail.com>
2021-09-26 19:46:38 -0300

    add calculo idade gestacional e calculo exame anterior testes

---
+++ backend/src/test/java/com/dev/tests/services/CalculoIdadeGestacionalAtual.java
@@ -0,0 +1,91 @@
+package com.dev.tests.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import javax.validation.ValidationException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.dev.dto.CalculoGestacionalDTO;
+import com.dev.dto.GestanteDTO;
+import com.dev.service.GestanteService;
+import com.dev.tests.Factory.DataValues;
+
+@ExtendWith(SpringExtension.class)
+public class CalculoIdadeGestacionalAtual {
+
+	@InjectMocks
+	private GestanteService service;
+
+	private DataValues dataValues;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		dataValues = new DataValues();
+	}
+
+
+	@Test
+	public void whenDaysIsMoreThan7ShouldReturnValidationException() {
+
+		Assertions.assertThrows(ValidationException.class, () -> {
+			CalculoGestacionalDTO calculoDTO = new CalculoGestacionalDTO(dataValues.dataAtualMenosUmDia, dataValues.semanas, dataValues.numeroDediasMaiorQue7,
+					dataValues.dataGestacionalAtual);
+			service.calcular(calculoDTO);
+		});
+
+	}
+
+	@Test
+	public void whenDaysIsMoreThan7ReturnTheCorrectErrorMessage() {
+
+		try {
+			CalculoGestacionalDTO calculoDTO = new CalculoGestacionalDTO(dataValues.dataAtualMenosUmDia, dataValues.semanas, dataValues.numeroDediasMaiorQue7,
+					dataValues.dataGestacionalAtual);
+			service.calcular(calculoDTO);
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "Número de dias devem ser menores do que 7!");
+		}
+	}
+	
+	@Test
+	public void whenWeeksIsMoreThan40ReturnValidationException() {
+
+		Assertions.assertThrows(ValidationException.class, () -> {
+			CalculoGestacionalDTO calculoDTO = new CalculoGestacionalDTO(dataValues.dataAtualMenosUmDia, dataValues.numeroDeSemanasMaiorQue40, dataValues.dias,
+					dataValues.dataGestacionalAtual);
+			service.calcular(calculoDTO);
+		});
+
+	}	
+	
+	@Test
+	public void whenWeeksIsMoreThan40ReturnTheCorrectErrorMessage() {
+
+		try {
+			CalculoGestacionalDTO calculoDTO = new CalculoGestacionalDTO(dataValues.dataAtualMenosUmDia, dataValues.numeroDeSemanasMaiorQue40, dataValues.dias,
+					dataValues.dataGestacionalAtual);
+			service.calcular(calculoDTO);
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "Número de semanas devem ser menores do que 40!");
+		}
+	}
+
+	@Test
+	public void whenDataIsValidCalcularShouldReturnCorrectDataProvavelDoParto() {
+		CalculoGestacionalDTO calculoDTO = new CalculoGestacionalDTO(dataValues.dataAtualMenosUmDia, dataValues.semanas, dataValues.dias,
+				dataValues.dataExameAnterior);
+		GestanteDTO dadosGestacionais = service.calcular(calculoDTO);
+
+		Assertions.assertEquals(LocalDate.now().plusDays(280).minusDays(111),
+				dadosGestacionais.getDataProvavelDoParto());
+	}
+
+}
